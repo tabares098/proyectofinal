bc7675a1114afd7bf4840a829da8df169c750f26
tabares098 <98791140+tabares098@users.noreply.github.com>
2022-06-07 23:05:34 -0500

    nueva falla corregida

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,4 +1,4 @@
-import React, { useEffect,useState } from 'react';
+import React, { useState } from 'react';
 import { useDispatch,useSelector } from 'react-redux';
 import "../styles/Home.css";
 import { Link } from 'react-router-dom';
